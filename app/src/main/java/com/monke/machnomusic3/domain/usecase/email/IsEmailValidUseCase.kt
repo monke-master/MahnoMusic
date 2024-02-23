f6c89ca77c6c4e34aeddf4c5af356107434c79f2
Igor Streshnev <monkemaster@yandex-team.ru>
2024-02-23 17:28:31 +0300

    IsEmailValidUseCase refactoring

--- app/src/main/java/com/monke/machnomusic3/domain/usecase/email/IsEmailValidUseCase.kt
+++ app/src/main/java/com/monke/machnomusic3/domain/usecase/email/IsEmailValidUseCase.kt
@@ -3,9 +3,7 @@ package com.monke.machnomusic3.domain.usecase.email
 import javax.inject.Inject
 
 class IsEmailValidUseCase @Inject constructor() {
-
     fun execute(email: String): Boolean {
         return email.isNotEmpty()
     }
-
 }
